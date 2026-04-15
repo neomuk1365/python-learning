94b9feb5cd2df20c3a96f657c7df9e321e10973f
Ravi Ranjan <Ravi@gmail.com>
2026-04-15 19:55:23 +0530

    message

--- chapter6_tryityourself3.py
+++ chapter6_tryityourself3.py
@@ -145,3 +145,107 @@ for pet in pets:
 import json
 print('\n')
 print(json.dumps(petowner,indent=4))
+'''
+6-9. Favorite Places: Make a dictionary called favorite_places. Think of three
+names to use as keys in the dictionary, and store one to three favorite places
+for each person. To make this exercise a bit more interesting, ask some friends
+to name a few of their favorite places. Loop through the dictionary, and print
+each person’s name and their favorite places.
+'''
+print('=========================================================')
+favorite_places=[['vipul','sikkim','puri','goa','deoghar'],['subham','darjeling','sikkim','goa'],['ravi','puri','digha'],['ravi','daman']]
+print(favorite_places)
+#converting flat data to heirarchy data using bottom up approach
+#output
+#{
+# 'vipul':
+# {
+# 'favourite_places':['sikkim','puri','goa']},
+#'subham':
+# {'favorite_places':['darjelling','sikkim','goa']},
+# 'ravi':{
+# 'favorite_places':['puri','digha']}
+#}
+nested_fvrt_places={}
+for places in favorite_places:
+    user=places[0]
+    print(user)
+    if user not in nested_fvrt_places:
+        nested_fvrt_places[user]={
+            'favorite_places':[],
+            'total_place':0
+        }
+    for i in range(1,len(places)):
+        #print(places[i])
+        nested_fvrt_places[user]['favorite_places'].append(places[i])
+        nested_fvrt_places[user]['total_place']+=1
+
+print(nested_fvrt_places)
+print(json.dumps(nested_fvrt_places,indent=4))
+print('another way of using .get()')
+for user in nested_fvrt_places:
+    #print(user)
+    print(f'{user}:{nested_fvrt_places.get(user,{}).get('favorite_places','[]')}')
+'''
+6-10. Favorite Numbers: Modify your program from Exercise 6-2 (page 102) so
+each person can have more than one favorite number. Then print each person’s
+name along with their favorite numbers.
+'''
+print('=============================================')
+favorite_numbers={
+    'marty byrde':5,
+    'wendy byrde':10,
+    'ruth langmore':13,
+    'darlen snell':45,
+    'jonah byrde':87
+}
+favorite_numbers1={
+    'marty byrde':
+    {'favorite_numbers':[5,3,2,8,7]},
+    'wendy byrde':
+     {'favorite_numbers':[10,3,2,8,7]},
+    'ruth langmore':
+    {'favorite_numbers':[13,3,2,5,67]},
+    'darlen snell':
+    {'favorite_numbers':[45,69,5,5,12]},
+    'jonah byrde':
+    {'favorite_numbers':[87,13,2,18]}
+}
+for names in favorite_numbers1:
+    #print(names)
+    print(f'{names}:')
+    for key, value in favorite_numbers1[names].items():
+        print(f'\t{key}:{value}')
+        for number in value:
+            print(number+1)
+print("=======================================================")
+'''
+6-11. Cities: Make a dictionary called cities. Use the names of three cities as
+keys in your dictionary. Create a dictionary of information about each city and
+include the country that the city is in, its approximate population, and one fact
+about that city. The keys for each city’s dictionary should be something like
+country, population, and fact. Print the name of each city and all of the information you have stored about it.
+'''
+city1,city2,city3='kolkata','ahmedabad','bengaluru'
+nested_dict_city={
+    city1:{
+        'country':'India',
+        'population':1.5 ,
+        'fact':'The treasure  art capital of india'
+    },
+    city2:{
+        'country':'India',
+        'population':1.8, #population in crore
+        'fact':'Business person city'
+    },
+    city3:{
+        'country':'India',
+        'population':2,
+        'fact':'Silicon valley of india'
+    }
+}
+for cities,cityinfo in nested_dict_city.items():
+    #print(cities)
+    print(f'{cities}:{cityinfo}')
+    for key,value in cityinfo.items():
+        print(f'\t{key} :{value}')
